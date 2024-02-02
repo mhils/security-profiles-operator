1e4f499c8acfd16092cefab078d8a07f463cb072
Maximilian Hils <mhils@google.com>
2024-02-02 14:53:36 +0000

    rust demobinary: improve error handling

--- main.rs
+++ main.rs
@@ -1,10 +1,11 @@
 use std::env;
-use std::fs::{File, OpenOptions};
-use std::io::{Read, Write};
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::net::{TcpListener, UdpSocket};
 
+use anyhow::{bail, Context, Result};
 use clap::Parser;
-use log::{error, info};
+use log::info;
 use std::process::Command;
 use std::str;
 
@@ -45,7 +46,7 @@ struct Args {
     subprocess: Vec<OsString>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     env_logger::Builder::new()
         .format(|buf, record| {
             writeln!(
@@ -66,50 +67,30 @@ fn main() {
 
     // File operations
     if args.file_write {
-        match File::create(TMPFILE) {
-            Ok(_) => info!("✅ File write successful: {}", TMPFILE),
-            Err(err) => error!("❌ Error creating file: {}", err),
-        }
+        OpenOptions::new()
+            .write(true)
+            .open(TMPFILE)
+            .context("❌ Failed to write file")?;
+        info!("✅ File write successful: {}", TMPFILE);
     }
 
     if args.file_read {
-        match OpenOptions::new().read(true).open(TMPFILE) {
-            Ok(mut file) => {
-                let mut contents = String::new();
-                if let Err(err) = file.read_to_string(&mut contents) {
-                    error!("❌ Error reading file: {}", err);
-                } else {
-                    info!("✅ File read successful: {}", TMPFILE);
-                }
-            }
-            Err(err) => error!("❌ Error reading file: {}", err),
-        }
+        OpenOptions::new()
+            .read(true)
+            .open(TMPFILE)
+            .context("❌ Failed to read file")?;
+        info!("✅ File read successful: {}", TMPFILE);
     }
 
     // Network operations
     if args.net_tcp {
-        match TcpListener::bind("0.0.0.0:0") {
-            // Dynamic port assignment
-            Ok(listener) => {
-                let addr = listener.local_addr().unwrap();
-                info!("✅ TCP server spawned: {}", addr);
-
-                // Listener would need to be handled, likely in a separate thread
-            }
-            Err(err) => error!("❌ Error starting TCP server: {}", err),
-        }
+        let listener = TcpListener::bind("0.0.0.0:0").context("❌ Failed to start TCP server")?;
+        info!("✅ TCP server spawned: {}", listener.local_addr()?);
     }
 
     if args.net_udp {
-        match UdpSocket::bind("0.0.0.0:0") {
-            Ok(socket) => {
-                let addr = socket.local_addr().unwrap();
-                info!("✅ UDP server spawned: {}", addr);
-
-                // Socket would need to be handled for receiving/sending
-            }
-            Err(err) => error!("❌ Error starting UDP server: {}", err),
-        }
+        let socket = UdpSocket::bind("0.0.0.0:0").context("❌ Failed to start UDP server")?;
+        info!("✅ UDP server spawned: {}", socket.local_addr()?);
     }
 
     if args.net_icmp {
@@ -118,26 +99,26 @@ fn main() {
 
     // Subprocess execution
     if let [program, program_args @ ..] = &args.subprocess[..] {
-        info!("✴️ Spawning subproces...");
-        match Command::new(program)
+        info!("✴️  Spawning {:?}...", args.subprocess);
+        let exit_code = Command::new(program)
             .args(program_args)
             .env(
                 "LOGPREFIX",
                 format!("  {}", env::var(LOGPREFIX_ENV_VAR).unwrap_or_default()),
             )
             .spawn()
-            .and_then(|mut child| child.wait())
-        {
-            Ok(code) => {
-                info!(
-                    "✅ {:?} terminated with exit code {}.",
-                    args.subprocess, code
-                );
-            }
-            Err(e) => {
-                error!("❌ Subprocess failed: {}", e)
-            }
-        };
+            .context("❌ Failed to spawn subprocess")?
+            .wait()
+            .context("❌ Subprocess failed")?;
+        if exit_code.success() {
+            info!(
+                "✅ {:?} terminated with exit code {}.",
+                args.subprocess, exit_code
+            );
+        } else {
+            bail!("❌ {:?} terminated with exit code {}.",args.subprocess, exit_code);
+        }
+        
     }
 
     // Simulating crash with SIGKILL (Caution!)
@@ -149,4 +130,5 @@ fn main() {
     }
 
     info!("⭐️ Success.");
+    Ok(())
 }
