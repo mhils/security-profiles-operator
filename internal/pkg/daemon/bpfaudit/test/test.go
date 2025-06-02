7f86450be70c60af6d772877a2cc6e0f17fd04bc
Maximilian Hils <mhils@google.com>
2025-06-02 11:43:22 +0000

    cleanup

--- internal/pkg/daemon/bpfaudit/test/test.go
+++ internal/pkg/daemon/bpfaudit/test/test.go
@@ -19,20 +19,3 @@ func main() {
 		logger.Info("violation", "line", line)
 	}
 }
-
-/*
-func getCgroupID() bpfaudit.CgroupId {
-	content, _ := os.ReadFile("/proc/self/cgroup")
-	re := regexp.MustCompile(`(?m)^0::(.*)$`)
-
-	matches := re.FindStringSubmatch(string(content))
-	cgroupPath := ""
-	if len(matches) > 1 {
-		cgroupPath = matches[1]
-	}
-
-	var stat syscall.Stat_t
-	_ = syscall.Stat(filepath.Join("/sys/fs/cgroup", cgroupPath), &stat)
-	return stat.Ino
-}
-*/
